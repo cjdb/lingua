8c6f5498a20951c39a90ab2156eca4a503053cfe
Christopher Di Bella <cjdb.ns@gmail.com>
2019-05-11 17:55:53 +0100

    implements the diagnostic level and diagnostic base types
    Closes #3
    
    From the design specification:
    
    >    All diagnostics should store a range of coordinates to source
    >    tokens, and a help message. There will be three diagnostic types:
    >    ill-formed, warning, and remark.
    
    Since the underlying implementation of one diagnostic is the same as
    another, it makes sense to have a common base for every diagnostic, to:
    
    1. implement [DRY]
    2. take advantage of [EBO]
    
    The base class will rely on a diagnostic level type, so it makes sense
    to couple it in #3.
    
    Diagnostics are intended to be privately derived from the diagnostic
    base, although the implementation has no way to make this guarantee.
    
    [DRY]: https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
    [EBO]: https://en.cppreference.com/w/cpp/language/ebo

---
+++ include/lingua/diagnostic/detail/diagnostic_base.hpp
@@ -0,0 +1,50 @@
+//
+//  Copyright 2019 Christopher Di Bella
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef LINGUA_DIAGNOSTIC_DETAIL_DIAGNOSTIC_BASE_HPP
+#define LINGUA_DIAGNOSTIC_DETAIL_DIAGNOSTIC_BASE_HPP
+
+#include "lingua/diagnostic/diagnostic_level.hpp"
+#include "lingua/source_coordinate_range.hpp"
+#include <string>
+#include <string_view>
+#include <utility>
+
+namespace lingua::detail_diagnostic {
+   template<diagnostic_level level_value>
+   class diagnostic_base {
+   public:
+      static inline constexpr auto level = level_value;
+
+      explicit diagnostic_base(source_coordinate_range const coordinates, std::string help_message) noexcept
+         : coordinates_{coordinates}
+         , help_message_{std::move(help_message)}
+      {
+      }
+
+      source_coordinate_range coordinates() const noexcept { return coordinates_; }
+
+      std::string_view help_message() const noexcept { return help_message_; }
+
+   protected:
+      ~diagnostic_base() = default;
+
+   private:
+      source_coordinate_range coordinates_;
+      std::string help_message_;
+   };
+}   // namespace lingua::detail_diagnostic
+
+#endif   // LINGUA_DIAGNOSTIC_DETAIL_DIAGNOSTIC_BASE_HPP
---
+++ include/lingua/diagnostic/diagnostic_level.hpp
@@ -0,0 +1,23 @@
+//
+//  Copyright 2019 Christopher Di Bella
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef LINGUA_DIAGNOSTIC_DIAGNOSTIC_LEVEL_HPP
+#define LINGUA_DIAGNOSTIC_DIAGNOSTIC_LEVEL_HPP
+
+namespace lingua {
+   enum class diagnostic_level { remark, warning, ill_formed };
+}   // namespace lingua
+
+#endif   // LINGUA_DIAGNOSTIC_DIAGNOSTIC_LEVEL_HPP
